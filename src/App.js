422ad2144301c6404a033a4312ec1cae6663fcdd
Thomas Morita <tym479@gmail.com>
2019-07-10 11:10:14 -0500

    added /components/Deck.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Deck from './components/Deck'
 
 function App() {
   return (
     <div className="App">
       <h1>Deck of Cards</h1>
+      <Deck />
     </div>
   );
 }
