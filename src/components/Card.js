46bd7b3be31e8d0e98180247567406344f9b9f31
Thomas Morita <tym479@gmail.com>
2019-07-10 11:28:53 -0500

    rendered the appropriate colors

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,10 @@ class Card extends React.Component {
         return (
             <div>
                 {this.props.cards.map((card) => (
-                    <h6>{card.value} {card.suit} {card.color}</h6>
+                    card.suit == '♥' || card.suit == '♦' ?
+                        <h6 style={{ color: 'red' }}>{card.value} {card.suit}</h6>
+                        :
+                        <h6 style={{ color: 'black' }}>{card.value} {card.suit}</h6>
                 ))}
             </div>
         );
